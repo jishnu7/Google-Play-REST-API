3736be2cbff0ac9561c9834c34c6d75a80d79087
lovesh <lovesh.bond@gmail.com>
2013-06-22 13:20:14 +0530

    Added rating count, description, screenshots

--- playapi.py
+++ playapi.py
@@ -47,6 +47,8 @@ def app_info(app_id, language='en_US'):
 
     data = BeautifulSoup(web)
     meta_data = data.find('div', {'class':'doc-metadata'})
+    desc_div = data.find('div', {'id':'doc-original-text'})
+    screenshots = data.findAll('img', {'itemprop': 'screenshot'})
 
     if meta_data == None:
         return not_found()
@@ -58,6 +60,7 @@ def app_info(app_id, language='en_US'):
     app_dev = app_dev_meta.find(itemprop='name')['content']
     app_dev_url = app_dev_meta.find(itemprop='url')['content']
     app_rating = meta_data.find(itemprop='ratingValue')['content']
+    app_rating_count = meta_data.find(itemprop='ratingCount')['content']
     app_updated = meta_data.find(itemprop='datePublished').next
     app_version = meta_data.find(itemprop='softwareVersion').next
     app_os = meta_data.find(itemprop='operatingSystems').next.next.next
@@ -65,6 +68,8 @@ def app_info(app_id, language='en_US'):
     app_size = meta_data.find(itemprop='fileSize').next
     app_price = meta_data.find(itemprop='price')['content']
     app_contentrating = meta_data.find(itemprop='contentRating').next
+    app_desc = desc_div.text
+    app_screenshots = [img['src'] for img in screenshots]
 
     app_data = {
             'name': app_name,
@@ -72,13 +77,16 @@ def app_info(app_id, language='en_US'):
             'author': app_dev,
             'author_url': url_play + app_dev_url,
             'ratingValue': app_rating,
+            'ratingCount': app_rating_count,
             'datePublished': app_updated,
             'softwareVersion': app_version,
             'operatingSystems': app_os,
             'numDownloads': app_downloads,
             'fileSize': app_size,
             'price': app_price,
-            'contentRating': app_contentrating
+            'contentRating': app_contentrating,
+            'description': app_desc,
+            'screenshots': app_screenshots,
             }
     resp = jsonify(app_data)
     resp.status_code = 200
